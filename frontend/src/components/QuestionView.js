8bd980dcfc0f01f4e5068d9e64e79aca2c30fcf2
Ibeh Esther <estheribeh5@gmail.com>
2022-06-16 21:49:57 +0100

    worked on a route handler and made some modifications throughout the project

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -83,7 +83,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/api/v1.0/questions`, //TODO: update request URL
+      url: `/api/v1.0/questions/search`, //TODO: update request URL
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',
